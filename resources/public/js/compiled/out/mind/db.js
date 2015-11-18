5b69ffa988dc8825612759255e9469ea603c9770
Andrzej <andrzej.fricze@codete.co>
2015-11-18 10:34:32 +0100

    Remove /resources from repo

--- resources/public/js/compiled/out/mind/db.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 0.0-3211 {}
-goog.provide('mind.db');
-goog.require('cljs.core');
-goog.require('reagent.core');
-mind.db.default_db = new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"name","name",1843675177),"re-frame",new cljs.core.Keyword(null,"thoughts","thoughts",1862571083),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"content","content",15833224),"first one",new cljs.core.Keyword(null,"id","id",-1388402092),(0)], null)], null),new cljs.core.Keyword(null,"thought-id","thought-id",-1135367546),(1)], null);
-
-//# sourceMappingURL=db.js.map?rel=1445010677349
